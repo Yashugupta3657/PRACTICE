61bcb0cbaf8def677114341906fae0ac566065a5
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-03-27 02:06:37 +0530

    Time: 2809 ms (5.00%), Space: 43.4 MB (39.38%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,24 +1,23 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int nLen = needle.length();
-        
-        if(nLen == 0){
-            return 0;
+           int h=haystack.length();
+        int n=needle.length();
+        if(n==0) return 0;
+        for(int i=0; i<h; i++){
+            if(haystack.charAt(i)==needle.charAt(0)){
+                if(n>1){
+                if(h-i>=n){
+                    for(int j=1; j<n; j++){
+                        if(haystack.charAt(i+j)!=needle.charAt(j)) break;
+                        if(j==n-1) return i;
+                    }
+                }
+                }else{
+                    return i;
+                }   
+          }
         }
-        
-        int hLen = haystack.length();
-        
-        if(hLen == 0 || hLen < nLen) {
-            return -1;
-        }
-        
-        int limit = hLen - nLen;
-        for(int i=0; i<=limit; i++) {
-            if(haystack.startsWith(needle, i)) {
-                return i;
-            }
-        }
-        
         return -1;
+    
     }
 }
