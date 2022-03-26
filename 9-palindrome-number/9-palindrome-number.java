432dda35dad7f8bc5234ad624333b4555fa868ec
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-03-27 01:03:26 +0530

    Time: 12 ms (70.57%), Space: 44.6 MB (52.87%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if (x<0 || (x%10 ==0 && x!=0)){
+            return false;
+        }
         int back=0;
         int front=x;
         while(x>0){
