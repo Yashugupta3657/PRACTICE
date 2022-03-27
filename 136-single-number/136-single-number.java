86857efe0c62aa8f2a728f791773fdff5bde241d
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-03-28 00:48:36 +0530

    Time: 16 ms (20.07%), Space: 53.5 MB (20.86%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,10 +1,21 @@
 class Solution {
     public int singleNumber(int[] nums) {
         //using xor
-        int sum=0;
+        // int sum=0;
+        // for(int i=0;i<nums.length;i++){
+        //     sum^=nums[i];
+        // }
+        // return sum;
+        HashSet<Integer> m=new HashSet<>();
         for(int i=0;i<nums.length;i++){
-            sum^=nums[i];
+           if(m.contains(nums[i])){
+               m.remove(nums[i]);
+           }
+            else
+                m.add(nums[i]);
         }
-        return sum;
+        for(int t:m)
+            return t;
+        return 0;
     }
 }
