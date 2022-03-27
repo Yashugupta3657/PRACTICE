69db73d59659714ba127288ba42bab4ff7a02b34
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-03-27 23:27:05 +0530

    Time: 6 ms (81.68%), Space: 61.7 MB (57.68%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.java
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.java
@@ -1,18 +1,32 @@
 import java.math.BigInteger;
 class Solution {
-    public List<Integer> addToArrayForm(int[] num, int k) {
-        List<Integer> mt=new ArrayList<>();
-        String str="";
-        for(int i=0;i<num.length;i++){
-            str+=num[i];
+    public List<Integer> addToArrayForm(int[] A, int k) {
+          int N = A.length;
+        int cur = k;
+        List<Integer> ans = new ArrayList();
+
+        int i = N;
+        while (--i >= 0 || cur > 0) {
+            if (i >= 0)
+                cur += A[i];
+            ans.add(cur % 10);
+            cur /= 10;
         }
-        BigInteger n=new BigInteger(str);
-        BigInteger ans=n.add(new BigInteger(String.valueOf(k)));
-        String str2=String.valueOf(ans);
-        for(int i=0;i<str2.length();i++)
-        {
-            mt.add(Integer.parseInt(str2.charAt(i)+""));
-        }
-        return mt;
+
+        Collections.reverse(ans);
+        return ans;
+        // List<Integer> mt=new ArrayList<>();
+        // String str="";
+        // for(int i=0;i<num.length;i++){
+        //     str+=num[i];
+        // }
+        // BigInteger n=new BigInteger(str);
+        // BigInteger ans=n.add(new BigInteger(String.valueOf(k)));
+        // String str2=String.valueOf(ans);
+        // for(int i=0;i<str2.length();i++)
+        // {
+        //     mt.add(Integer.parseInt(str2.charAt(i)+""));
+        // }
+        // return mt;
     }
 }
