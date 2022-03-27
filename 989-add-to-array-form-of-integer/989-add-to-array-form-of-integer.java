96f6a901b2bddbccb6deb535360cab5bbd533cf5
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-03-27 23:25:10 +0530

    Time: 309 ms (5.04%), Space: 171.5 MB (5.00%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.java
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.java
@@ -7,9 +7,7 @@ class Solution {
             str+=num[i];
         }
         BigInteger n=new BigInteger(str);
-        BigInteger ans;
-        String str1=String.valueOf(k);
-        ans=n.add(new BigInteger(str1));
+        BigInteger ans=n.add(new BigInteger(String.valueOf(k)));
         String str2=String.valueOf(ans);
         for(int i=0;i<str2.length();i++)
         {
