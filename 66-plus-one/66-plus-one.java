76fdb0a2fbf8e799f3ef850361da8f0e0f544be7
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-03-27 16:57:57 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (19.11%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,18 +1,31 @@
 import java.math.BigInteger;
 class Solution {
     public int[] plusOne(int[] digits) {
-        String str="";
-        for(int i=0;i<digits.length;i++){
-            str+=digits[i];
+        // String str="";
+        // for(int i=0;i<digits.length;i++){
+        //     str+=digits[i];
+        // }
+        // BigInteger total;
+        // BigInteger a = new BigInteger(str);
+        // total=a.add(new BigInteger("1"));
+        // String str1=String.valueOf(total);
+        // int arr[]=new int[str1.length()];
+        // for(int i=0;i<str1.length();i++){
+        //     arr[i]=Integer.parseInt(str1.charAt(i)+"");
+        // }
+        // return arr;
+        if(digits.length==0)
+            return null;
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
         }
-        BigInteger total;
-        BigInteger a = new BigInteger(str);
-        total=a.add(new BigInteger("1"));
-        String str1=String.valueOf(total);
-        int arr[]=new int[str1.length()];
-        for(int i=0;i<str1.length();i++){
-            arr[i]=Integer.parseInt(str1.charAt(i)+"");
-        }
-        return arr;
+        int newArr[]=new int[digits.length+1];
+        newArr[0]=1;
+        newArr[1]=0;
+        return newArr;
     }
 }
