1e87302640a9820b55d4a998e7b030c6f7c54af5
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-03-28 14:38:37 +0530

    Time: 7 ms (59.89%), Space: 48.9 MB (20.72%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -1,59 +1,74 @@
 //2 stacks
+// class MinStack {
+// Stack<Integer> value;
+//     Stack<Integer> minV; 
+//     public MinStack() {
+//         value=new Stack<>();
+//         minV=new Stack<>();
+//     }
+    
+//     public void push(int val) {
+//         value.push(val);
+//         if(minV.isEmpty()){
+//             minV.push(val);
+//         }
+//         else if(minV.peek()>=val){
+//             minV.push(val);
+//         }
+//     }
+    
+//     public void pop() {
+//         if(value.peek().equals(minV.peek()))
+//             minV.pop();
+//         value.pop();
+//     }
+    
+//     public int top() {
+//         return value.peek();
+//     }
+    
+//     public int getMin() {
+//         return minV.peek();
+//     }
+// }
+//using pair
 class MinStack {
-Stack<Integer> value;
-    Stack<Integer> minV; 
+   class Pair{
+        int val;
+        int min;
+        public Pair(int val,int min){
+            this.val=val;
+            this.min=min;
+        }
+    }
+    Stack<Pair> ans;
     public MinStack() {
-        value=new Stack<>();
-        minV=new Stack<>();
+        ans=new Stack<>();
     }
     
     public void push(int val) {
-        value.push(val);
-        if(minV.isEmpty()){
-            minV.push(val);
+        if(ans.isEmpty())
+            ans.push(new Pair(val,val));
+        else if(ans.peek().min<=val){
+            ans.push(new Pair(val,ans.peek().min));
         }
-        else if(minV.peek()>=val){
-            minV.push(val);
+        else{
+            ans.push(new Pair(val,val));
         }
     }
     
     public void pop() {
-        if(value.peek().equals(minV.peek()))
-            minV.pop();
-        value.pop();
+        ans.pop();
     }
     
     public int top() {
-        return value.peek();
+        return ans.peek().val;
     }
     
     public int getMin() {
-        return minV.peek();
+        return ans.peek().min;
     }
 }
-//using pair
-// class MinStack {
-
-//     public MinStack() {
-        
-//     }
-    
-//     public void push(int val) {
-        
-//     }
-    
-//     public void pop() {
-        
-//     }
-    
-//     public int top() {
-        
-//     }
-    
-//     public int getMin() {
-        
-//     }
-// }
 
 /**
  * Your MinStack object will be instantiated and called as such:
