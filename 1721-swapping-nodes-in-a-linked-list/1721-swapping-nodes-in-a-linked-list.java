e4421998d766b2aaade21f641a3014a0d5db540e
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-04-04 09:20:34 +0530

    Time: 2 ms (100.00%), Space: 183.3 MB (27.34%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,43 +10,65 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        if(head.next==null)
-            return head;
-        ListNode temp=head;;
-        int size=1;
-        int store1=0;
-        int store2=0;
-        while(temp!=null){
-            if(size==k){
-                store1=temp.val; 
-            }
-            if(temp.next!=null)
-            size++;
-            temp=temp.next;
+        ListNode fast = head;
+        ListNode slow = head;
+        for (int i = 1; i < k; i += 1) {
+            fast = fast.next;
         }
-        int till=size-k+1;
-        temp=head;
-        size=1;
-        while(temp!=null){
-            if(size==till)
-            {
-                store2=temp.val;
-                temp.val=store1;
-                break;
-            }
-            size++;
-            temp=temp.next;
+        ListNode front = fast;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
         }
-        temp=head;
-        size=1;
-        while(temp!=null){
-            if(size==k){
-                temp.val=store2;
-                break;
-            }
-            size++;
-            temp=temp.next;
-        }
+        swapVal(front, slow);
         return head;
     }
+    
+    public void swapVal(ListNode node1, ListNode node2) {
+        int temp = node1.val;
+        node1.val = node2.val;
+        node2.val = temp;
+    }  
 }
+// class Solution {
+//     public ListNode swapNodes(ListNode head, int k) {
+//         if(head.next==null)
+//             return head;
+//         ListNode temp=head;;
+//         int size=1;
+//         int store1=0;
+//         int store2=0;
+//         while(temp!=null){
+//             if(size==k){
+//                 store1=temp.val; 
+//             }
+//             if(temp.next!=null)
+//             size++;
+//             temp=temp.next;
+//         }
+//         int till=size-k+1;
+//         temp=head;
+//         size=1;
+//         while(temp!=null){
+//             if(size==till)
+//             {
+//                 store2=temp.val;
+//                 temp.val=store1;
+//                 break;
+//             }
+//             size++;
+//             temp=temp.next;
+//         }
+//         temp=head;
+//         size=1;
+//         while(temp!=null){
+//             if(size==k){
+//                 temp.val=store2;
+//                 break;
+//             }
+//             size++;
+//             temp=temp.next;
+//         }
+//         return head;
+//     }
+// }
