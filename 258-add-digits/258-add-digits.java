03e10c813b3a1bfe23d69ae046d13c040d1b4af5
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-04-04 12:35:30 +0530

    Time: 2 ms (62.04%), Space: 41.1 MB (67.15%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -19,8 +19,9 @@
 // }
 class Solution {
     public int addDigits(int num) {
-        if (num == 0) return 0;
-        if (num % 9 == 0) return 9;
-        return num % 9;
+        while(num / 10 > 0){
+            num = num / 10 + num % 10;
+        }
+        return num;
     }
 }
