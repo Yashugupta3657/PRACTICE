8b4e5e8f7143ad4dc585e7d641be002e7212a40d
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-04-04 13:22:10 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (98.85%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,15 +1,24 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int arr[]=new int[nums[nums.length-1]+1];
-        for(int i=0;i<nums.length;i++){
-            arr[nums[i]]++;
-        }
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]==0)
-                return i;
-        }
-        return nums[nums.length-1]+1;
+        //using simple maths formula
+        int sum=(nums.length*(nums.length+1))/2;
+        for(int i=0;i<nums.length;i++)
+            sum-=nums[i];
+        return sum;
     }
 }
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         Arrays.sort(nums);
+//         int arr[]=new int[nums[nums.length-1]+1];
+//         for(int i=0;i<nums.length;i++){
+//             arr[nums[i]]++;
+//         }
+//         for(int i=0;i<arr.length;i++)
+//         {
+//             if(arr[i]==0)
+//                 return i;
+//         }
+//         return nums[nums.length-1]+1;
+//     }
+// }
