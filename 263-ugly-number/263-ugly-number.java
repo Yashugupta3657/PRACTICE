50aa26e9e4b5ba644a8f83d0f0e17c185c21aa8c
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-04-04 12:52:03 +0530

    Time: 1 ms (94.49%), Space: 41.9 MB (10.73%) - LeetHub

--- 263-ugly-number/263-ugly-number.java
+++ 263-ugly-number/263-ugly-number.java
@@ -5,6 +5,7 @@ class Solution {
       while(n%2==0) n/=2;
         while(n%3==0) n/=3;
         while(n%5==0) n/=5;
-        return n==1;
+        if(n==1) return true;
+        return false;
     }
 }
