09113cd02fce4e523d868499d6a594d25fce3c3b
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-04-06 16:02:56 +0530

    Time: 43 ms (46.37%), Space: 41.7 MB (87.10%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int threeSumMulti(int[] arr, int target) {
+        long count=0;
+        int i=0;
+        int MOD = 1_000_000_007;
+        Arrays.sort(arr);
+        while(i<arr.length){
+            int low=i+1;
+            int high=arr.length-1;
+            while(low<high){
+                if(arr[i]+arr[low]+arr[high]==target&&arr[low]!=arr[high]){
+                    int left=1;
+                    int right=1;
+                    while(low+1<high&&arr[low]==arr[low+1]){
+                        left++;
+                        low++;
+                    }
+                    while(high-1>low&&arr[high]==arr[high-1]){
+                        right++;
+                        high--;
+                    }
+                    count+=left*right;
+                    count%=MOD;
+                    low++;
+                    high--;
+                }
+                else if(arr[i]+arr[low]+arr[high]==target){
+                    count+=((high-low+1)*(high-low))/2;
+                    count%=MOD;
+                    break;
+                }
+                else if(arr[i]+arr[low]+arr[high]>target){
+                    high--;
+                }
+                else{
+                    low++;
+                }
+            }
+                  i++;
+        }
+        return (int)count;
+    }
+}
