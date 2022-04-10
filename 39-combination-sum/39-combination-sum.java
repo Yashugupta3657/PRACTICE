bb6affda7bc44479daa7d4f682f4055c70bab486
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-04-10 12:20:03 +0530

    Time: 8 ms (36.13%), Space: 45.1 MB (34.53%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -5,16 +5,16 @@ class Solution {
         return maa;
     }
     public void find(int[] can,int target,int ind,List<Integer> m){
-        if(check(m)>target||ind==can.length){
+        if(target<0||ind==can.length){
             return;
         }
-        else if(m.size()!=0&&check(m)==target){
+        else if(target==0){
             maa.add(new ArrayList<Integer>(m));
             return;
         }
         else{
             m.add(can[ind]);
-            find(can,target,ind,m);
+            find(can,target-can[ind],ind,m);
             m.remove(m.size()-1);
             find(can,target,ind+1,m);
         }
@@ -26,3 +26,32 @@ class Solution {
         return sum;
     }
 }
+
+// class Solution {
+//     List<List<Integer>> maa=new ArrayList<>();
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         find(candidates,target,0,new ArrayList<Integer>());
+//         return maa;
+//     }
+//     public void find(int[] can,int target,int ind,List<Integer> m){
+//         if(check(m)>target||ind==can.length){
+//             return;
+//         }
+//         else if(m.size()!=0&&check(m)==target){
+//             maa.add(new ArrayList<Integer>(m));
+//             return;
+//         }
+//         else{
+//             m.add(can[ind]);
+//             find(can,target,ind,m);
+//             m.remove(m.size()-1);
+//             find(can,target,ind+1,m);
+//         }
+//     }
+//     public int check(List<Integer> m){
+//         int sum=0;
+//         for(int i=0;i<m.size();i++)
+//             sum+=m.get(i);
+//         return sum;
+//     }
+// }
