c1d2d9c4d93bd4cc6837dfc25c8b7dadea3941cb
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-04-10 12:14:34 +0530

    Time: 20 ms (9.20%), Space: 44.6 MB (64.58%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    List<List<Integer>> maa=new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        find(candidates,target,0,new ArrayList<Integer>());
+        return maa;
+    }
+    public void find(int[] can,int target,int ind,List<Integer> m){
+        if(check(m)>target||ind==can.length){
+            return;
+        }
+        else if(m.size()!=0&&check(m)==target){
+            maa.add(new ArrayList<Integer>(m));
+            return;
+        }
+        else{
+            m.add(can[ind]);
+            find(can,target,ind,m);
+            m.remove(m.size()-1);
+            find(can,target,ind+1,m);
+        }
+    }
+    public int check(List<Integer> m){
+        int sum=0;
+        for(int i=0;i<m.size();i++)
+            sum+=m.get(i);
+        return sum;
+    }
+}
