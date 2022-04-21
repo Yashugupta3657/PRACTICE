f49f38f219d3c03debf6db6f0ba547a4025c4f2c
Yashu Gupta <60153103+Yashugupta3657@users.noreply.github.com>
2022-04-21 19:16:14 +0530

    Time: 69 ms (29.04%), Space: 110.2 MB (16.26%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,24 +1,41 @@
 class MyHashMap {
-    int size=(int)Math.pow(10,6)+1;
-    int arr[];
-    public MyHashMap() {
-        arr=new int[size];
-        Arrays.fill(arr,-1);
-    }
+//     int size=(int)Math.pow(10,6)+1;
+//     int arr[];
+//     public MyHashMap() {
+//         arr=new int[size];
+//         Arrays.fill(arr,-1);
+//     }
     
-    public void put(int key, int value) {
-        arr[key]=value;
-    }
+//     public void put(int key, int value) {
+//         arr[key]=value;
+//     }
     
-    public int get(int key) {
-        if(arr[key]!=-1)
-        return arr[key];
-        return -1;
-    }
+//     public int get(int key) {
+//         if(arr[key]!=-1)
+//         return arr[key];
+//         return -1;
+//     }
     
-    public void remove(int key) {
-        arr[key]=-1;
-    }
+//     public void remove(int key) {
+//         arr[key]=-1;
+//     }
+    int[] values = new int[1000001];
+    
+        public MyHashMap() {
+        }
+
+        public void put(int key, int value) {
+            values[key] = value + 1;
+        }
+
+        public int get(int key) {
+            return values[key] - 1;
+        }
+
+        public void remove(int key) {
+            values[key] = 0;
+        }
+
 }
 
 /**
